18f2276c99f3cbd964e3a4ff4f43c0b2e029a587
yasindu12 <yasindusamarasinghe10@gmail.com>
2025-04-20 18:46:19 +0530

    add error logs

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,7 @@
 const CustomError = require('../errors/custom-error');
 
 const errorHandler = (err, req, res, next) => {
+    console.log(err)
     if (err instanceof CustomError) {
         return res.status(400).json({ success: false, message: err.message });
     }
